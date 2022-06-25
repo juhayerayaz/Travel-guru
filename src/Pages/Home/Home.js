a78082d99543932e3123e0c3fb9904537b24eebc
juhayerayaz <aiazj97@gmail.com>
2022-06-25 13:17:07 +0600

    Basic project setup done

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../../../../Assignment-12/drilled-client-site/src/Shared/Navbar';
+
+const Home = () => {
+    return (
+        <Navbar></Navbar>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Navbar/Navabar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navabar = () => {
+    return (
+        <div>
+            <h1>HEllo</h1>
+        </div>
+    );
+};
+
+export default Navabar;
