cf169411c9cad49eefdf6413651651ddf35ccb8b
juhayerayaz <aiazj97@gmail.com>
2022-06-26 22:56:17 +0600

    fixed banner issue

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -22,9 +22,9 @@ const Banner = () => {
                     spaceBetween={30}
                     navigation={true}
                 >
-                    <SwiperSlide className='slide-1'>
+                    <SwiperSlide className='swiper-slider'>
                         {({ isActive }) => (
-                            <div>{isActive ? <div>
+                            <div>{isActive ? <div className='slide-1'>
                                 <div className='relative ml-[-600px]'>
                                     <h1 className='text-7xl text-white font-bold'>Cox's Bazaar</h1>
                                     <p className='text-left w-[455px] my-4 text-white'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and...</p>
@@ -33,9 +33,7 @@ const Banner = () => {
                                 <div className='slide-bg border-4 border-primary'>
                                     <h1 className='text-2xl absolute bottom-8 left-4 font-bold text-white tracking-wider'>COX'S BAZAAR</h1>
                                 </div>
-                            </div> : <div className='slide-bg'>
-                                <h1 className='text-2xl absolute bottom-8 left-4 font-bold text-white tracking-wider'>COX'S BAZAAR</h1>
-                            </div>}</div>
+                            </div> : <></>}</div>
                         )}
                     </SwiperSlide>
                     <SwiperSlide className='slide-2'>
